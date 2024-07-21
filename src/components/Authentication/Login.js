75bd1aa039cb549f224ec228b221b700a0198210
pritamkumarbishwas <pritamkumarbishwas85@gmail.com>
2024-07-21 22:19:38 +0530

    e

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -46,7 +46,7 @@ const Login = () => {
         },
       };
 
-      const { data } = await axios.post(
+      const { data } = await axios.get(
         '/api/user/login', // Ensure this endpoint is correct
         { email, password },
         config
